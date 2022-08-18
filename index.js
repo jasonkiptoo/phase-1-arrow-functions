17f923ecaf2a56f0623f2d7b74f87a8ed3cfb931
jasonkiptoo <kiptoojason@gmail.com>
2022-08-18 11:50:07 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let divide=function divide(){
+
+    // return 2000/100
+    const div=(parameter1,parameter2)=>parameter1/parameter2
+
+    return div(2000,100);
+}
+
+const square=(parameter1)=>parameter1*parameter1
+
+const add=(parameter1,parameter2)=>parameter1+parameter2
